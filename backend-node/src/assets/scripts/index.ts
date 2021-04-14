de70875788c2fab4791c3cbb25c3beba6805f2ca
Ramon Borges <ramontricolor08@hotmail.com>
2021-04-14 17:49:03 -0300

    Configurar proxy.

--- backend-node/src/assets/scripts/index.ts
+++ backend-node/src/assets/scripts/index.ts
@@ -1,3 +1,6 @@
 import { createConnection } from "typeorm";
 
 createConnection();
+
+// Create a db on docker
+// docker run --name thedailyscripturedb -e POSTGRES_PASSWORD=root -e POSTGRES_DB=thedailyscripture -p 5432:5432 -d postgres
